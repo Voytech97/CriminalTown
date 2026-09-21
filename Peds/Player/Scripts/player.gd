4389ed6eae5b3a4570bdc0aff1d657df1fc7eaf5
Wojciech 165431 <chx104461@student.chorzow.merito.pl>
2026-09-22 00:26:30 +0200

    added player gravity and ability to change it through the inspector

--- Peds/Player/Scripts/player.gd
+++ Peds/Player/Scripts/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 
 @export var speed = 2
 @export var rotation_speed = 2.5
+@export var gravity = 10
 
 var rotation_direction = 0
 
@@ -11,12 +12,18 @@ var camera_rotation
 func _ready():
 	camera_rotation = camera.global_rotation
 
+	
+
 func get_input():
 	rotation_direction = Input.get_axis("ui_right", "ui_left")
 	velocity = basis.x * Input.get_axis("ui_down", "ui_up") * speed
 
 func _physics_process(delta):
 	get_input()
+	
+	if not is_on_floor():
+		velocity += get_gravity() * gravity * delta
+		
 	rotate_y(rotation_direction * rotation_speed * delta)
 	move_and_slide()
 	
